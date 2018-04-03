c48365935acf68095f3fb54d6327d8792040fbc8
Rahul Rajaram <rajaram.rahul91@gmail.com>
2018-04-04 00:04:07 +1000

    including beautify extensions

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 var gulp = require('gulp');
 var util = require('gulp-util');
-var jsBeautify = require('js-beautify');
+var jsBeautify = require('js-beautify').js_beautify;
 var git = require('simple-git')();
+var fs = require('fs');
 
 gulp.task('default', function() {
     console.log("Hello World!");
-    git.diff(['--cached', '--name-only', '--diff-filter=ACM'], function (err, results) {
-        var aFiles = results.trim().split('\n');
-        for (var i = 0; i < aFiles.length; i++) {
-            var sFile = aFiles[i];
-            console.log(sFile);
-            jsBeautify(sFile);
-        }
-    });
-    throw new util.PluginError('Test', 'Plugin Error', {showStack: false});
+    // git.diff(['--cached', '--name-only', '--diff-filter=ACM'], function (err, results) {
+    //     var aFiles = results.trim().split('\n');
+    //     for (var i = 0; i < aFiles.length; i++) {
+    //         var sFile = aFiles[i];
+    //         var data = fs.readFileSync(sFile, 'utf8');
+    //         console.log(data);
+    //         var newData = jsBeautify(sFile);
+    //         if (sFile.indexOf('js') > -1 && data.toString() !== newData.toString()) {
+    //             // throw new util.PluginError('Test', 'Ensure VSCode Format is turned on', { showStack: false });
+    //         }
+    //     }
+    // });
 });
