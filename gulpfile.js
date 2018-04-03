065cd312b40758676bd51cd1254c1d465f6d7628
Rahul Rajaram <rajaram.rahul91@gmail.com>
2018-04-04 00:03:16 +1000

    test pre-commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 var gulp = require('gulp');
 var util = require('gulp-util');
+var jsBeautify = require('js-beautify');
+var git = require('simple-git')();
 
 gulp.task('default', function() {
     console.log("Hello World!");
+    git.diff(['--cached', '--name-only', '--diff-filter=ACM'], function (err, results) {
+        var aFiles = results.trim().split('\n');
+        for (var i = 0; i < aFiles.length; i++) {
+            var sFile = aFiles[i];
+            console.log(sFile);
+            jsBeautify(sFile);
+        }
+    });
     throw new util.PluginError('Test', 'Plugin Error', {showStack: false});
 });
